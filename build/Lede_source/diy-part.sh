c78a33c072c52ca7051cf3ffa373014ccf19f75e
FinisLee <68887579+FinisLee@users.noreply.github.com>
2021-10-02 17:26:58 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -8,9 +8,9 @@
 
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 # 删除老argon
-rm -rf package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon
 # 拉取argon主题
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
